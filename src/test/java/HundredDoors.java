db8f86e52dd677929f42d4640c0d6547da227740
Sebastian Larsson <sebastian.larsson@cybercom.com>
2014-09-01 21:40:24 +0200

    green

--- src/test/java/HundredDoors.java
+++ src/test/java/HundredDoors.java
@@ -57,7 +57,7 @@ public class HundredDoors {
 	private void visit(List<Door> doors, int jumpSequence) {
 		for (int index = 0; index < doors.size(); index++) {
 			if(index % jumpSequence == 0)
-				doors.get(index).isOpen = true;
+				doors.get(index).isOpen = !doors.get(index).isOpen;
 		}
 	}
 }
