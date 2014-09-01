296e76f37ff7a022ebeaa8271617ff16a61d0023
Sebastian Larsson <sebastian.larsson@cybercom.com>
2014-09-01 19:42:16 +0200

    green

--- src/test/java/HundredDoors.java
+++ src/test/java/HundredDoors.java
@@ -9,7 +9,15 @@ public class HundredDoors {
 	// 100 closed doors
 	// 100 opened doors
 	// every 2nd door
-	private final List<Door> allClosed = null;
+	@SuppressWarnings("serial")
+	private static final List<Door> allClosed = new ArrayList<Door>() {
+		{
+			for (int i = 0; i < 100; i++) {
+				add(new Door(false));
+			}
+		}
+	};
+
 	@Test
 	public void a_hundred_closed_doors() {
 		List<Door> doors = new ArrayList<>(allClosed);
