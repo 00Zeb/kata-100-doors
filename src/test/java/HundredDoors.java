e3f91e73fc37d9e46650d52961e436dd266d3049
Sebastian Larsson <sebastian.larsson@cybercom.com>
2014-09-01 19:45:36 +0200

    red visit each door

--- src/test/java/HundredDoors.java
+++ src/test/java/HundredDoors.java
@@ -10,6 +10,14 @@ public class HundredDoors {
 	// 100 opened doors
 	// every 2nd door
 	@SuppressWarnings("serial")
+	private static final List<Door> allOpen = new ArrayList<Door>() {
+		{
+			for (int i = 0; i < 100; i++) {
+				add(new Door(true));
+			}
+		}
+	};
+	@SuppressWarnings("serial")
 	private static final List<Door> allClosed = new ArrayList<Door>() {
 		{
 			for (int i = 0; i < 100; i++) {
@@ -17,18 +25,37 @@ public class HundredDoors {
 			}
 		}
 	};
-
-	@Test
-	public void a_hundred_closed_doors() {
-		List<Door> doors = new ArrayList<>(allClosed);
-		assertEquals(allClosed, doors);
-	}
-
+	
 	private static class Door {
 		boolean isOpen;
 
 		public Door(boolean isOpen) {
 			this.isOpen = isOpen;
 		}
+
+		@Override
+		public String toString() {
+			return String.valueOf(isOpen);
+		}
 	}
+
+	@Test
+	public void a_hundred_closed_doors() {
+		List<Door> doors = new ArrayList<>(allClosed);
+		assertEquals(allClosed, doors);
+	}
+	
+	@Test
+	public void visit_each_door() {
+		List<Door> doors = new ArrayList<>(allClosed);
+		visit(doors);
+		assertEquals(allOpen, doors);
+	}
+
+	private void visit(List<Door> doors) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
